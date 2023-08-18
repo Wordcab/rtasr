2766ae9e24fbefbffe2353426175bb213c54d7fa
chainyo <thomas@wordcab.com>
2023-08-18 11:08:09 +0200

    init benchmark cmd and create list command

--- src/rtasr/__main__.py
+++ src/rtasr/__main__.py
@@ -10,28 +10,104 @@ from rich_argparse import RichHelpFormatter
 
 install(show_locals=True)
 
+from rtasr.cli_messages import ascii_art, error_message
+from rtasr.constants import DATASETS, PROVIDERS
 
-ascii_art = r"""
-  ___      _         _____ _         _       _   ___ ___
- | _ \__ _| |_ ___  |_   _| |_  __ _| |_    /_\ / __| _ \
- |   / _` |  _/ -_)   | | | ' \/ _` |  _|  / _ \\__ |   /
- |_|_\__,_|\__\___|   |_| |_||_\__,_|\__| /_/ \_|___|_|_\
- _________________________________________________________
- by Wordcab
-"""
+
+def benchmark_asr_command_factory(args: argparse.Namespace):
+    return BenchmarkASRCommand(args.provider, args.dataset, args.output_dir, args.no_cache)
 
 
 def download_dataset_command_factory(args: argparse.Namespace):
     return DownloadDatasetCommand(args.dataset, args.output_dir, args.no_cache)
 
 
+def list_items_command_factory(args: argparse.Namespace):
+    return ListItemsCommand(args.type)
+
+
+class BenchmarkASRCommand:
+    """Benchmark an ASR provider against a dataset."""
+
+
+    @staticmethod
+    def register_subcommand(parser: argparse.ArgumentParser) -> None:
+        subparser = parser.add_parser(
+            "benchmark", help="Benchmark an ASR provider against a dataset."
+        )
+        subparser.add_argument(
+            "-p",
+            "--provider",
+            help="The ASR provider to benchmark.",
+            required=True,
+            type=str,
+        )
+        subparser.add_argument(
+            "-d", "--dataset", help="The dataset to use.", required=True, type=str
+        )
+        subparser.add_argument(
+            "-o",
+            "--output_dir",
+            help=(
+                "Path to store the downloaded files. Defaults to"
+                " `~/.cache/rtasr/datasets`."
+            ),
+            required=False,
+            default=None,
+            type=str,
+        )
+        subparser.add_argument(
+            "--no-cache",
+            help="Whether to use the cache or not.",
+            required=False,
+            action="store_false",
+        )
+        subparser.set_defaults(func=benchmark_asr_command_factory)
+
+    def __init__(
+        self,
+        provider: str,
+        dataset: str,
+        output_dir: Union[str, None] = None,
+        use_cache: bool = True,
+    ) -> None:
+        """Initialize the command."""
+        self.provider = provider
+        self.dataset = dataset
+        self.output_dir = output_dir
+        self.use_cache = use_cache
+
+    def run(self) -> None:
+        """Run the command."""
+        try:
+            if self.provider.lower() not in PROVIDERS:
+                print(error_message.format(input_type="provider", user_input=self.provider))
+                print("".join([f"  - [bold]{p}[bold]\n" for p in PROVIDERS]))
+                exit(1)
+            if self.dataset.lower() not in DATASETS:
+                print(error_message.format(input_type="dataset", user_input=self.dataset))
+                print("".join([f"  - [bold]{d}[bold]\n" for d in DATASETS]))
+                exit(1)
+
+            if self.provider.lower() == "deepgram":
+                pass
+            elif self.provider.lower() == "wordcab":
+                pass
+            else:
+                raise NotImplementedError(
+                    "The provider must be either `deepgram` or `wordcab`."
+                )
+        except KeyboardInterrupt:
+            print("\n[bold red]Cancelled by user.[/bold red]\n")
+            exit(1)
+        except Exception as e:
+            raise Exception(e) from e
+
 class DownloadDatasetCommand:
     """Download a dataset."""
 
-    DATASETS = ["ami", "voxconverse"]
-
     @staticmethod
-    def register_subcommand(parser: argparse.ArgumentParser):
+    def register_subcommand(parser: argparse.ArgumentParser) -> None:
         subparser = parser.add_parser("download", help="Download a dataset.")
         subparser.add_argument(
             "-d", "--dataset", help="The dataset to download.", required=True, type=str
@@ -82,20 +158,51 @@ class DownloadDatasetCommand:
                 )
 
             else:
-                print(
-                    f"[bold red]Error: The dataset `{self.dataset}` is not"
-                    " supported.[/bold red]\n[bold"
-                    " red]==================================================================[/bold"
-                    " red]\n"
-                )
-                print("Do you mean one of these datasets?\n")
-                print("".join([f"  - [bold]{d}[bold]\n" for d in self.DATASETS]))
+                print(error_message.format(input_type="dataset", user_input=self.dataset))
+                print("".join([f"  - [bold]{d}[bold]\n" for d in DATASETS]))
                 exit(1)
         except KeyboardInterrupt:
             print("\n[bold red]Cancelled by user.[/bold red]\n")
             exit(1)
         except Exception as e:
             raise Exception(e) from e
+
+class ListItemsCommand:
+    """List items."""
+
+    @staticmethod
+    def register_subcommand(parser: argparse.ArgumentParser) -> None:
+        """Register the subcommand."""
+        subparser = parser.add_parser("list", help="List items.")
+        subparser.add_argument(
+            "-t",
+            "--type",
+            help="The type of items to list.",
+            type=str,
+            default=None,
+        )
+        subparser.set_defaults(func=list_items_command_factory)
+
+    def __init__(self, item_type: Union[str, None] = None) -> None:
+        """Initialize the command."""
+        self.item_type = item_type
+
+    def run(self) -> None:
+        """Run the command."""
+        if self.item_type is None:
+            print("Datasets:")
+            print("".join([f"  - [bold]{d}[bold]\n" for d in DATASETS]))
+            print("Providers:")
+            print("".join([f"  - [bold]{p}[bold]\n" for p in PROVIDERS]))
+            exit(1)
+        if self.item_type.lower() == "datasets":
+            print("".join([f"  - [bold]{d}[bold]\n" for d in DATASETS]))
+        elif self.item_type.lower() == "providers":
+            print("".join([f"  - [bold]{p}[bold]\n" for p in PROVIDERS]))
+        else:
+            print(error_message.format(input_type="item type", user_input=self.item_type))
+            print("`datasets` or `providers`")
+            exit(1)
 
 
 def main() -> None:
@@ -109,7 +216,9 @@ def main() -> None:
     commands_parser = parser.add_subparsers(help="rtasr command helpers")
 
     # Register subcommands
+    BenchmarkASRCommand.register_subcommand(commands_parser)
     DownloadDatasetCommand.register_subcommand(commands_parser)
+    ListItemsCommand.register_subcommand(commands_parser)
 
     args = parser.parse_args()
 

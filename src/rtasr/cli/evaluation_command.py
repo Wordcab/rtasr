bacdf7a066fe5e235e2930d62bfe149179e71cd4
chainyo <thomas@wordcab.com>
2023-08-28 15:14:38 +0200

    add exit to the evaluation command

--- src/rtasr/cli/evaluation_command.py
+++ src/rtasr/cli/evaluation_command.py
@@ -149,9 +149,11 @@ class EvaluationCommand:
 
             if _metric not in DATASETS[_dataset]["metrics"]:
                 print(
-                    f"Metric {_metric} not supported for dataset {_dataset}."
-                    " Please check `rtasr list -t datasets` for more info."
+                    f"[bold red]Metric {_metric} not supported for dataset {_dataset}."
+                    " [/bold red]\nPlease check `rtasr list -t datasets` for more"
+                    " info."
                 )
+                exit(1)
 
             if self.dataset_dir is None:
                 dataset_dir = resolve_cache_dir() / "datasets" / _dataset
